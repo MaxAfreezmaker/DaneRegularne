6ae27cf0f55048b9282f8e9d8b925ae877789b17
maksymilian.adamski <maksymilian.adamski@HK-21>
2023-10-29 15:09:12 +0100

    Add project files.

---
+++ DaneRegularne/Controllers/DataController.cs
@@ -0,0 +1,33 @@
+﻿using DaneRegularne.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+
+namespace DaneRegularne.Controllers
+{
+    public class DataController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Form()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Form(DataModel dane)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Wynik", dane);
+            }
+            else return View();
+
+        }
+        public IActionResult Wynik(DataModel dane)
+        {
+            return View(dane);
+        }
+    }
+}
---
+++ DaneRegularne/Models/DataModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DaneRegularne.Models
+{
+    public class DataModel
+    {
+        [Required(ErrorMessage = "Uzupełnij pole swoim imieniem.")]
+        public string Imie { get; set; }
+        [Required(ErrorMessage = "Uzupełnij pole swoim nazwiskiem.")]
+        public string Nazwisko { get; set; }
+
+        [Required(ErrorMessage = "Prosze podaj Email")]
+        [RegularExpression(".+\\@.+\\.[a-z]{2,3}")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Prosze podaj Temat")]
+        public string Temat { get; set; }
+        [Required(ErrorMessage = "Prosze podaj Tresc wiadomosci")]
+        [MinLength(10), MaxLength(50)]
+        public string Tresc { get; set; }
+    }
+}
